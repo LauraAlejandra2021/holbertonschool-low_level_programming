8c2c8141082db53fa8fa989595ebdc00b1fc3f3f
LauraAlejandra2021 <3450@holbertonschool.com>
2021-06-23 11:44:00 -0700

    R y =

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,24 +1,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-
 /**
  *main -Positive or negative.
  *
- *return: 0 as success
+ *@n: is a number int
+ *
+ *return: always 0
 */
 int main(void)
 {
 int n;
-
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-
 if (n > 0)
+{
 printf("%d is positive\n", n);
+}
 else if (n < 0)
+{
 printf("%d is negative\n", n);
+}
 else
+{
 printf("%d is zero\n", n);
-Return (0)
+}
+return (0);
 }
