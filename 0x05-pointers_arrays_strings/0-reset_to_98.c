1c2c4b788d83bcace4b89b33b81f9de3165d5065
LauraAlejandra2021 <3450@holbertonschool.com>
2021-06-29 21:36:17 -0700

    prrincipal

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+# incluye  " holberton.h "
+
+/ * *
+
+ * reset_to_98 - variable enviada a la función en 98
+
+* @n: puntero al entero enviado a la función
+
+ *
+
+* Devolución: no
+
+* /
+
+anular  reset_to_98 ( int *n)
+{
+	* n = 98;
+}
