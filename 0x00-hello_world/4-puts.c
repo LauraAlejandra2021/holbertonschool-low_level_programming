b37856ec2ca2299a17a703696eb7d7f0e4901c52
LauraAlejandra2021 <3450@holbertonschool.com>
2021-07-22 20:05:32 -0500

    repaso y correciones

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - Entrypoint
+ * main - Funcion principal
  *
  *Return: Always 0 (Success)
  */
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
