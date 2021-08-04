32bdb91d786c4550e3df037693abb3ef3be254b0
LauraAlejandra2021 <3450@holbertonschool.com>
2021-08-04 12:31:54 -0500

    correcion

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,9 +1,11 @@
 #include "lists.h"
+
 /**
 *print_list - function that prints all the elements of a list
 *@h: pointer
 *Return: number of the list
 */
+
 size_t print_list(const list_t *h)
 {
 	size_t i = 0;
