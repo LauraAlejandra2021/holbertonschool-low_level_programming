7a8287d42ef3009a852ebee28c9222494217b0ab
LauraAlejandra2021 <3450@holbertonschool.com>
2021-08-04 21:17:03 -0500

    agregar un nodo al final de una lista

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,20 +7,20 @@
 */
 list_t *add_node(list_t **head, const char *str)
 {
-        list_t *new;
-        int i = 0;
-        new = malloc(sizeof(list_t));
-        if (new == NULL)
-        {
-                return (NULL);
-        }
-        while (str[i] != '\0')
-        {
-                i++;
-        }
-        new->str = strdup(str);
-        new->len = i;
-        new->next = (*head);
-        (*head) = new;
-        return (new);
+list_t *new;
+int i = 0;
+new = malloc(sizeof(list_t));
+if (new == NULL)
+{
+return (NULL);
+}
+while (str[i] != '\0')
+{
+i++;
+}
+new->str = strdup(str);
+new->len = i;
+new->next = (*head);
+(*head) = new;
+return (new);
 }
