9283da324f0136ca37362670d469f04a91c49df9
LauraAlejandra2021 <3450@holbertonschool.com>
2021-08-04 20:23:24 -0500

    Punto 2

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -9,7 +9,6 @@
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
- *
  * Description: singly linked list node structure
  */
 typedef struct list_s
